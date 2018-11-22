aaa74d778f9994b2731d02e86b99262d04e8db86
nihongzheng <nhz@fuhenkeji.com>
2018-11-23 01:08:23 +0800

    add spring cloud

--- edu/spring-cloud/spring-cloud-config-server/src/main/java/com/nee/spring/cloud/config/server/SpringCloudConfigServer.java
+++ edu/spring-cloud/spring-cloud-config-server/src/main/java/com/nee/spring/cloud/config/server/SpringCloudConfigServer.java
@@ -3,7 +3,15 @@ package com.nee.spring.cloud.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.environment.Environment;
+import org.springframework.cloud.config.environment.PropertySource;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.config.server.environment.EnvironmentRepository;
+import org.springframework.context.annotation.Bean;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 @EnableConfigServer
@@ -11,5 +19,25 @@ public class SpringCloudConfigServer {
 
     public static void main(String args[]) {
         SpringApplication.run(SpringCloudConfigServer.class, args);
+    }
+
+
+    @Bean
+    public EnvironmentRepository environmentRepository() {
+        return (application, profile, label) -> {
+
+            Environment environment = new Environment ("default", profile);
+
+
+            List<PropertySource> propertySources = environment.getPropertySources();
+
+
+            Map<String, String> map = new HashMap<>();
+            map.put("name", "heikki");
+            PropertySource propertySource = new PropertySource("map", map);
+            propertySources.add(propertySource);
+            return environment;
+
+        };
     }
 }
